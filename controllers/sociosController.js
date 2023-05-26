c608c1ff53b887dc0af55f88478f9db461b781e5
estefano99 <estefanobugari@gmail.com>
2023-05-26 18:28:58 -0300

    actualizar socios y limite de datos

--- controllers/sociosController.js
+++ controllers/sociosController.js
@@ -4,22 +4,21 @@ import sanitize from "mongo-sanitize";
 const autenticarSocio = async (req, res) => {
   //Valida que no haya una inyeccion noSQL
   const dni = sanitize(req.body.dni);
-
+  
   //Consultar si existe el socio en la base de datos
   const socio = await Socio.findOne({ dni }).select("tipoUsuario dni");
-
+  
   //Si el socio no existe muestra un error
   if (!socio) {
     const error = new Error("El socio no existe");
     return res.status(401).json({ msg: error.message });
-  };
+  }
 
   return res.send(socio);
 };
 
 //Devuelve los datos del socio para mostrar en el perfil.
-const mostrarPerfil = async (req,res) => {
-
+const mostrarPerfil = async (req, res) => {
   try {
     const { id } = req.params;
     const socio = await Socio.findById(id).select("nombreCompleto");
@@ -31,15 +30,15 @@ const mostrarPerfil = async (req,res) => {
     }
 
     res.send(socio);
-    
   } catch (error) {
     return res.status(401).json({ msg: error });
   }
-}
+};
 
 // Devolver un socio al leer el QR
 const devolverSocio = async (req, res) => {
-  const {id} = req.body;
+  const { id } = req.body;
+
   try {
     const socio = await Socio.findById(id);
 
@@ -48,20 +47,22 @@ const devolverSocio = async (req, res) => {
       const error = new Error("El socio no existe");
       return res.status(401).json({ msg: error.message });
     }
-    
+
     res.send(socio);
   } catch (error) {
     const errorCreado = new Error("Socio no registrado");
-    return res.status(401).json({ msg: errorCreado.message});
+    return res.status(401).json({ msg: errorCreado.message });
   }
 };
 
 //El admin quiere consultar todos los socios
 const obtenerSocios = async (req, res) => {
-  const tipoUsuario = 'socio';
+  const tipoUsuario = "socio";
 
   try {
-    const listaSocios = await Socio.find({ tipoUsuario }).sort({nombreCompleto: 'asc'});
+    const listaSocios = await Socio.find({ tipoUsuario }).sort({
+      nombreCompleto: "asc",
+    });
     res.send(listaSocios);
   } catch (error) {
     return res.status(401).json({ msg: error.message });
@@ -69,57 +70,61 @@ const obtenerSocios = async (req, res) => {
 };
 
 //El admin carga el archivo de socios y se actualiza en la base de datos
-let contadorUpdate = 0;
-let contadorCreate = 0;
-let huboError = false; //En caso de que haya un socio duplicado retorna el mensaje de ese socio y no el de que salio todo bien.
-let sociosDuplicados = []; //Contiene los socios que tienen duplicado el Codigo y que se valida por DNI.
-let contador = 0
-
 const cargarArchivo = async (req, res) => {
   const body = req.body;
 
-  for (const socio of body) {
-    const { dni, codigo, nombreCompleto, cuotasAdeudadas } = socio;
+  const todosLosSociosDB = await Socio.find(
+    { tipoUsuario: "socio" },
+    "dni codigo nombreCompleto cuotasAdeudadas"
+    ); // Trae todos los socios de la base de datos.
+    
+    const bodyMapeado = body.map((socio) => ({
+      dni: String(socio.dni),
+      codigo: String(socio.codigo),
+      cuotasAdeudadas: parseInt(socio.cuotasAdeudadas),
+      nombreCompleto: socio.nombreCompleto,
+    }));
 
-    try {
-      const existeSocio = await Socio.findOne({ dni });
+    //Contiene los socios a actualizar, Pero de la BD, por lo tanto no las cuotas nuevas que vienen del front
+    const sociosActualizar = todosLosSociosDB.filter((socio) => {
+      return bodyMapeado.some((s) => s.dni === socio.dni);
+    });
 
-      if (existeSocio) {
-        await Socio.updateOne({ dni }, { cuotasAdeudadas });
-        contadorUpdate++
-      } else {
-        contadorCreate++
-        await Socio.create({ dni, codigo, nombreCompleto, cuotasAdeudadas });
-      }
-    } catch (error) {
-      console.log(`Error: ${error}`);
-      sociosDuplicados.push(socio)
-      huboError = true;
-      contador++
-      console.log("catch " + contador)
-      // return res.status(500).send({ msg: error.message });
-    }
-    console.log("actualizados: " + contadorUpdate)
-    console.log("creados: " +contadorCreate)
-  };
+    //Contiene los socios Nuevos, osea los que se van a crear
+    const sociosNuevos = bodyMapeado.filter((socio) => {
+      return !todosLosSociosDB.some((s) => s.dni === socio.dni);
+    });
 
-  console.log(sociosDuplicados)
+  try {
+    // Construir el objeto de actualización para actualizar masivamente, el metodo find en update es porque necesito pasarle las cuotas nuevas que coincidan con el dni de sociosActualizar
+    const actualizaciones = sociosActualizar.map((socio) => ({
+      updateOne: {
+        filter: { dni: socio.dni },
+        update: {
+          $set: {
+            cuotasAdeudadas: bodyMapeado.find((s) => s.dni === socio.dni)
+              .cuotasAdeudadas,
+          },
+        },
+      },
+    }));
 
-  // Devolver respuesta al front
-  if (!huboError) {
-    res.send({msg: "Los socios se han creado o actualizado con exito!"});
-  }
+    // Actualizar los socios existentes
+    const resultado = await Socio.bulkWrite(actualizaciones);
 
-  if (huboError) {
-    res.send({msg:`Se actualizaron o crearon con exito, pero existen dni erroneos`, sociosDuplicados})
+    const respuesta2 = await Socio.insertMany(sociosNuevos);
+
+    res.send({ msg: "Los socios se han creado o actualizado con éxito!" });
+  } catch (error) {
+    console.log(`Error: ${error}`);
+    res.status(500).send({ msg: error.message });
   }
 };
-
 
 export {
   autenticarSocio,
   obtenerSocios,
   cargarArchivo,
   mostrarPerfil,
-  devolverSocio
+  devolverSocio,
 };
--- index.js
+++ index.js
@@ -6,7 +6,9 @@ import cors from 'cors';
 import bodyParser from 'body-parser';
 
 const app = express();
-app.use(express.json());
+app.use(express.json({ limit: '20mb' }));
+app.use(express.urlencoded({ limit: '20mb', extended: true }));
+
 dotenv.config();
 
 conectarDB();
@@ -24,9 +26,6 @@ const corsOptions = {
 }
 app.use(cors(corsOptions));
 // app.use(cors({ origin: '*' }));
-
-app.use(bodyParser.json({ limit: '50mb' }));
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
 app.use("/", socioRoutes);
 
